00cc5ce1f1de8372f58cf92fd40bd11ca0791890
jsong96 <woon7743@gmail.com>
2021-04-07 04:28:00 -0500

    p4 done, p5 start

---
+++ .history/main_20210403142150.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+
+  int a1 = 5, a2 = 8;
+
+  cout << "a1 = " << a1 << "\ta2 = " << a2 << '\n';
+
+  // legal call
+
+  Func(a1, a2);
+
+  cout << "a1 = " << a1 << "\ta2 = " << a2 << '\n';
+
+  // legal call
+
+  Func(a1, 20);
+
+  cout << "a1 = " << a1 << "\ta2 = " << a2 << '\n';
+
+  return 0;
+}
+
+void Func(int &x, int y)
+
+{
+
+  x = x * 2;
+
+  y = y + 2;
+
+  cout << "x = " << x << '\n';
+
+  cout << "y = " << y << '\n';
+}
